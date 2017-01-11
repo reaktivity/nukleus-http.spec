76897a97013060de67fec6929faac20527915fba
John Fallows <john.fallows@kaazing.com>
2017-01-11 10:27:15 -0800

    Initial commit

---
+++ src/test/java/org/reaktivity/specification/nukleus/http/streams/server/rfc7230/ConnectionManagementIT.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2016-2017 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.specification.nukleus.http.streams.server.rfc7230;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+import org.reaktivity.specification.nukleus.NukleusRule;
+
+public class ConnectionManagementIT
+{
+    private final K3poRule k3po = new K3poRule()
+            .addScriptRoot("streams", "org/reaktivity/specification/nukleus/http/streams/rfc7230/connection.management")
+            .addScriptRoot("http", "org/kaazing/specification/http/rfc7230/connection.management");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    private final NukleusRule nukleus = new NukleusRule()
+        .directory("target/nukleus-itests")
+        .streams("http", "source")
+        .streams("rejectTarget", "http#source")
+        .streams("target", "http#source")
+        .streams("http", "replySource")
+        .streams("replyTarget", "http#replySource");
+
+    @Rule
+    public final TestRule chain = outerRule(nukleus).around(k3po).around(timeout);
+
+    @Test
+    @Specification({
+//      "${http}/response.status.101.with.upgrade/request",
+        "${streams}/response.status.101.with.upgrade/server/source",
+        "${streams}/response.status.101.with.upgrade/server/nukleus",
+        "${streams}/response.status.101.with.upgrade/server/target" })
+    public void shouldSwitchProtocolAfterUpgrade() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_INITIAL");
+        k3po.notifyBarrier("ROUTED_REPLY");
+        k3po.finish();
+    }
+}
