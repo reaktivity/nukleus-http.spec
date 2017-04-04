a654e7ce785dd382eb42a9e315c5135f0434bf22
Chris Barrow <chris.barrow@kaazing.com>
2017-04-04 14:50:09 -0800

    Applied review comments: made HTTP options configurable. Raw scripts now have no knowledge of nukleus transport.

--- src/test/java/org/reaktivity/specification/nukleus/http/streams/server/rfc7230/newsystem/ArchitectureIT.java
+++ src/test/java/org/reaktivity/specification/nukleus/http/streams/server/rfc7230/newsystem/ArchitectureIT.java
@@ -43,6 +43,12 @@ public class ArchitectureIT
     @Rule
     public final TestRule chain = outerRule(nukleus).around(k3po).around(timeout);
 
+    /*
+     * The two high-level scripts are running against each other,
+     * so are operating on the same streams files.
+     * client is writing to target/source, reading from source/target
+     * server is reading from target/source, writing to target/source
+    */
     @Test
     @Specification({
         "${streams}/request.and.response/cooked/client",
@@ -50,18 +56,84 @@ public class ArchitectureIT
     @ScriptProperty({"target \"target\", source \"source\""})
     public void shouldCorrelateRequestAndResponseCooked() throws Exception
     {
-        k3po.start();
         k3po.finish();
     }
 
+    /*
+     * The two raw scripts are running against each other,
+     * so are operating on the same streams files.
+     * client is writing to target/source, reading from source/target
+     * server is reading from target/source, writing to source/target
+    */
     @Test
     @Specification({
         "${streams}/request.and.response/raw/client",
         "${streams}/request.and.response/raw/server"})
-    @ScriptProperty({"target \"target\", source \"source\""})
+    @ScriptProperty({
+        "httpClientOptions {\"http:transport\":\"nukleus://streams/target\"," +
+                      "\"nukleus:source\":\"source\"," +
+                      "\"nukleus:route\":${nuklei:newReferenceId()}," +
+                      "\"nukleus:window\":\"8192\"," +
+                      "\"nukleus:transmission\":\"duplex\"}",
+        "httpServerOptions {\"http:transport\":\"nukleus://streams/target\"," +
+                      "\"nukleus:source\":\"source\"," +
+                      "\"nukleus:route\":${nuklei:newReferenceId()}," +
+                      "\"nukleus:window\":\"8192\"," +
+                      "\"nukleus:transmission\":\"duplex\"}"
+
+    })
     public void shouldCorrelateRequestAndResponseRaw() throws Exception
     {
-        k3po.start();
+        k3po.finish();
+    }
+
+    /*
+     * The raw client script is simulating like an http request coming in in raw form
+     * from tcp nukleus into http/source, so writing the raw request into http/source
+     * and reading the (encoded, raw) response from source/http.
+     * The cooked server script is reading that decoded request in high level form
+     * from target/http and writing the high-level response back to http/target.
+    */
+    @Test
+    @Specification({
+        "${route}/input/new/controller",
+        "${streams}/request.and.response/cooked/server",
+        "${streams}/request.and.response/raw/client"})
+    @ScriptProperty({
+        "source \"http\"",
+        "httpClientOptions {\"http:transport\":\"nukleus://streams/http\"," +
+                      "\"nukleus:source\":\"source\"," +
+                      "\"nukleus:route\":${newSourceInputRef}," + // property created by controller script
+                      "\"nukleus:window\":\"8192\"," +
+                      "\"nukleus:transmission\":\"duplex\"}"
+    })
+    public void testHttpNukleusAsServer() throws Exception
+    {
+        k3po.finish();
+    }
+
+    /*
+     * The raw server script is behaving like an encoded http request going out to
+     * to transport nukleus in target/http, so reading the request from target/http
+     * and reading the (encoded) response into http/target.
+     * The cooked client script is writing the high-level HTTP request to
+     * http/source and reading the decoded high-level response from source/http.
+    */
+    @Test
+    @Specification({
+        "${route}/output/new/controller",
+        "${streams}/request.and.response/raw/server",
+        "${streams}/request.and.response/cooked/client"})
+    @ScriptProperty({
+        "source \"http\"",
+        "httpServerOptions {\"http:transport\":\"nukleus://streams/http\"," +
+                      "\"nukleus:source\":\"source\"," +
+                      "\"nukleus:route\":${newTargetOutputRef}," + // property created by controller script
+                      "\"nukleus:window\":\"8192\"," +
+                      "\"nukleus:transmission\":\"duplex\"}"
+    })
+    public void testHttpNukleusAsClient() throws Exception
+    {
         k3po.finish();
     }
 
