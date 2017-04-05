27cfecd7e07b6091087e62e2c01f8ca1d55e4fd9
Jitendra Kotamraju <jitsni@gmail.com>
2017-04-05 10:53:07 -0700

    Testcase for HTTP2 stream containing multiple DATA frames HTTP2 DATA frame (0 length + END_STREAM) is sent when nukleus sends high-level end frame

--- src/test/java/org/reaktivity/specification/nukleus/http2/streams/server/rfc7540/ConnectionManagementIT.java
+++ src/test/java/org/reaktivity/specification/nukleus/http2/streams/server/rfc7540/ConnectionManagementIT.java
@@ -108,4 +108,17 @@ public class ConnectionManagementIT
         k3po.notifyBarrier("ROUTED_OUTPUT");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+            "${streams}/multiple.data.frames/server/source",
+            "${streams}/multiple.data.frames/server/nukleus",
+            "${streams}/multiple.data.frames/server/target" })
+    public void multipleDataFrames() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_INPUT");
+        k3po.notifyBarrier("ROUTED_OUTPUT");
+        k3po.finish();
+    }
 }
