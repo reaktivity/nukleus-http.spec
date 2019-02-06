06b7cc77beac70423a91ba4b356182223d091808
Akram Yakubov <akram.yakubov@kaazing.com>
2019-02-05 16:25:00 -0800

    Test case for request's Host header have port

--- src/test/java/org/reaktivity/specification/http/rfc7230/ConnectionManagementIT.java
+++ src/test/java/org/reaktivity/specification/http/rfc7230/ConnectionManagementIT.java
@@ -451,4 +451,16 @@ public class ConnectionManagementIT
         k3po.notifyBarrier("ROUTED_SERVER");
         k3po.finish();
     }
+
+    @Test
+    @Specification({
+            "${scripts}/request.authority.with.port/client",
+            "${scripts}/request.authority.with.port/server" })
+    @ScriptProperty("serverTransport \"nukleus://streams/http#0\"")
+    public void requestAuthorithWithPort() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.finish();
+    }
 }
