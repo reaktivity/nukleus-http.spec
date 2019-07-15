0ad27c000e1a87d90aaa1ff7a501e681cb5b4e96
John Fallows <john.fallows@kaazing.com>
2019-07-15 14:52:47 -0700

    Add support for route header overrides

--- src/test/java/org/reaktivity/specification/nukleus/http2/streams/rfc7540/ConnectionManagementIT.java
+++ src/test/java/org/reaktivity/specification/nukleus/http2/streams/rfc7540/ConnectionManagementIT.java
@@ -53,6 +53,19 @@ public class ConnectionManagementIT
     @Test
     @ScriptProperty("serverTransport \"nukleus://streams/http2#0\"")
     @Specification({
+            "${streams}/http.get.exchange.with.header.override/client",
+            "${streams}/http.get.exchange.with.header.override/server"
+    })
+    public void shouldSendRequestWithHeaderOverride() throws Exception
+    {
+        k3po.start();
+        k3po.notifyBarrier("ROUTED_CLIENT");
+        k3po.finish();
+    }
+
+    @Test
+    @ScriptProperty("serverTransport \"nukleus://streams/http2#0\"")
+    @Specification({
             "${streams}/http.post.exchange/client",
             "${streams}/http.post.exchange/server"
     })
